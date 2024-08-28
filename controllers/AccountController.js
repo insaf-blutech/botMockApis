b346f7cdb16a764dc532d23c47ca65c339dbc424
insaf-blutech <insaf.btc@gmail.com>
2024-08-28 12:59:21 +0500

    payee api changes

--- controllers/AccountController.js
+++ controllers/AccountController.js
@@ -6,9 +6,9 @@ const TransactionModel = require("../models/transactionModel");
 
 class AccountController {
   async AddPayeeAccount(req, res) {
-    const { session_id, account_holder_name, bankName } = req.body;
+    const { session_id, account_holder_name, bankName, payeeName } = req.body;
     try {
-      if (!session_id || !account_holder_name || !bankName) {
+      if (!session_id || !account_holder_name || !bankName || payeeName) {
         throw new Error("All fields Required!!!");
       }
       const accountNumber = Number(req.query.accountNumber);
--- models/accountModel.js
+++ models/accountModel.js
@@ -6,6 +6,7 @@ const AccountSchema = new Schema(
     session_id: { type: Number, required: true },
     account_holder_name: { type: String, required: true },
     bankName: { type: String, required: true },
+    payeeName: { type: String, required: true },
     balance: { type: Number, required: true, default: 45000 },
     currency: { type: String, required: true, default: "Pkr" },
   },
