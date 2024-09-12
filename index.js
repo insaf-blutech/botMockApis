642f8fad4d03dd25eab6e66b573899bd2beb3fb1
insaf-blutech <insaf.btc@gmail.com>
2024-09-12 13:16:30 +0500

    netlify Deployment config + vercel json path change

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const fs = require("fs");
+const app = express();
+const path = require("path");
+const cors = require("cors");
+
+const balanceRoutes = require("../../routes/balanceRoutes");
+const accountRoutes = require("../../routes/accountRoutes");
+const generalRoutes = require("../../routes/generalRoutes");
+const fundRoutes = require("../../routes/fundRoutes");
+const dbConnection = require("../../config/db-config");
+
+app.use(express.json());
+app.use(cors());
+
+dbConnection();
+
+app.get("/", (req, res) => {
+  console.log("GET REQUEST : ", req.body);
+  res.send("<h1>Hello From Serverless</h1>");
+});
+
+app.use("/api/general", generalRoutes);
+app.use("/api/account", accountRoutes);
+app.use("/api/balance", balanceRoutes);
+app.use("/api/fund", fundRoutes);
+
+app.listen(8000, () => {
+  console.log("Server is live at 8000.....");
+});
+module.exports.handler = serverless(app);
