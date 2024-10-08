a79a0bad9b517d89395619dc61dfdf07d516414e
insaf-blutech <insaf.btc@gmail.com>
2024-10-08 13:58:40 +0500

    timestamps added to url api

--- models/urlModel.js
+++ models/urlModel.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const urlSchema = new Schema({
-  routeUrl: {
-    type: String,
-    required: true,
+const urlSchema = new Schema(
+  {
+    routeUrl: {
+      type: String,
+      required: true,
+    },
   },
-});
+  { timestamps: true }
+);
 
 const UrlModel = mongoose.model("Url", urlSchema);
 
